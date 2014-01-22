3c28ba57007ee66d883ca423426235078076c78b
Stefano Valicchia <stefano.valicchia@gmail.com>
2014-01-22 22:07:45 +0100

    some mods

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,8 @@
 'use strict';
 require('newrelic');
 var statik = require('statik');
-var server = statik.createServer('.');
+var server = statik.createServer('.', {
+    cache: 7200,
+    gzip: true
+});
 server.listen();
